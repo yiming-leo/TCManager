578189923684cc6f3c173a281fa51e9363c2d76c
Hooorus <2479711422@qq.com>
2022-11-16 22:29:22 +0800

    打通医院发往邮政的接口

---
+++ TCMPost/src/main/java/cn/calendo/tcmpost/TcmPostApplication.java
@@ -0,0 +1,13 @@
+package cn.calendo.tcmpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcmPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcmPostApplication.class, args);
+    }
+
+}
