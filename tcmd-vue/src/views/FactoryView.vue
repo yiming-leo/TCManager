ef15a2b14b677747a5acf221546bf37905b588a5
Hooorus <2479711422@qq.com>
2022-12-02 16:09:43 +0800

    跑通前端

---
+++ tcmd-vue/src/views/FactoryView.vue
@@ -0,0 +1,386 @@
+<template>
+  <div class="factory">
+    <a-table :data-source="data" :columns="columns">
+      <div
+          slot="filterDropdown"
+          slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
+          style="padding: 8px"
+      >
+        <a-input
+            v-ant-ref="c => (searchInput = c)"
+            :placeholder="`Search ${column.dataIndex}`"
+            :value="selectedKeys[0]"
+            style="width: 188px; margin-bottom: 8px; display: block;"
+            @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+            @pressEnter="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
+        />
+        <a-button
+            type="primary"
+            icon="search"
+            size="small"
+            style="width: 90px; margin-right: 8px"
+            @click="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
+        >
+          Search
+        </a-button>
+        <a-button size="small" style="width: 90px" @click="() => handleReset(clearFilters)">
+          Reset
+        </a-button>
+      </div>
+      <a-icon
+          slot="filterIcon"
+          slot-scope="filtered"
+          type="search"
+          :style="{ color: filtered ? '#108ee9' : undefined }"
+      />
+      <template slot="customRender" slot-scope="text, record, index, column">
+      <span v-if="searchText && searchedColumn === column.dataIndex">
+        <template
+            v-for="(fragment, i) in text
+            .toString()
+            .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
+        >
+          <mark
+              v-if="fragment.toLowerCase() === searchText.toLowerCase()"
+              :key="i"
+              class="highlight"
+          >{{ fragment }}</mark>
+          <template v-else>{{ fragment }}</template>
+        </template>
+      </span>
+        <template v-else>
+          {{ text }}
+        </template>
+      </template>
+      <template slot="operation" slot-scope="text, record">
+        <a-button type="primary">分配药厂</a-button>
+      </template>
+    </a-table>
+  </div>
+</template>
+<script>
+const data = [
+  {
+    key: '1',
+    id: 1234567890,
+    date: '2022-05-17',
+    time: '17:08:29',
+    patientName: 'John Brown',
+    patientAge: 32,
+    patientGender: '男',
+    doctorName: '张医生',
+    doctorID: '0128',
+    outpatientNo: '4023',
+    price: 13.25,
+  },
+  {
+    key: '2',
+    id: 1234567891,
+    date: '2022-05-18',
+    time: '09:18:33',
+    patientName: 'Joe Black',
+    patientAge: 42,
+    patientGender: '女',
+    doctorName: '张医生',
+    doctorID: '0128',
+    outpatientNo: '4023',
+    price: 14.11,
+  },
+  {
+    key: '3',
+    id: 1234567892,
+    date: '2022-05-18',
+    time: '07:28:21',
+    patientName: 'Jim Green',
+    patientAge: 32,
+    patientGender: '男',
+    doctorName: '张医生',
+    doctorID: '0128',
+    outpatientNo: '4023',
+    price: 76.32,
+  },
+  {
+    key: '4',
+    id: 1234567893,
+    date: '2022-10-27',
+    time: '12:07:12',
+    patientName: 'Jim Red',
+    patientAge: 32,
+    patientGender: '女',
+    doctorName: '张医生',
+    doctorID: '0128',
+    outpatientNo: '4023',
+    price: 73.21,
+  },
+];
+export default {
+  components: {},
+  data() {
+    return {
+      data,
+      searchText: '',
+      searchInput: null,
+      searchedColumn: '',
+      columns: [
+        {
+          title: '序列号',
+          dataIndex: 'id',
+          key: 'id',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.id
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              }, 0);
+            }
+          },
+        },
+        {
+          title: '开方日期',
+          dataIndex: 'date',
+          key: 'date',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.date
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              }, 0);
+            }
+          },
+        },
+        {
+          title: '开方时间',
+          dataIndex: 'time',
+          key: 'time',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.time
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              }, 0);
+            }
+          },
+        },
+        {
+          title: '病人姓名',
+          dataIndex: 'patientName',
+          key: 'patientName',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.patientName
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              }, 0);
+            }
+          },
+        },
+        {
+          title: '病人年龄',
+          dataIndex: 'patientAge',
+          key: 'patientAge',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.patientAge
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: '病人性别',
+          dataIndex: 'patientGender',
+          key: 'patientGender',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.patientGender
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: '医生姓名',
+          dataIndex: 'doctorName',
+          key: 'doctorName',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.doctorName
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: '医生ID',
+          dataIndex: 'doctorID',
+          key: 'doctorID',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.doctorID
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: '门诊号',
+          dataIndex: 'outpatientNo',
+          key: 'outpatientNo',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.outpatientNo
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: '交易金额（RMB）',
+          dataIndex: 'price',
+          key: 'price',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.price
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: '操作',
+          dataIndex: 'operation',
+          key: 'operation',
+          scopedSlots: {customRender: 'operation'},
+          // scopedSlots: {
+          //   filterDropdown: 'filterDropdown',
+          //   filterIcon: 'filterIcon',
+          //   customRender: 'customRender',
+          // },
+          // onFilter: (value, record) =>
+          //     record.price
+          //         .toString()
+          //         .toLowerCase()
+          //         .includes(value.toLowerCase()),
+          // onFilterDropdownVisibleChange: visible => {
+          //   if (visible) {
+          //     setTimeout(() => {
+          //       this.searchInput.focus();
+          //     });
+          //   }
+          // },
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSearch(selectedKeys, confirm, dataIndex) {
+      confirm();
+      this.searchText = selectedKeys[0];
+      this.searchedColumn = dataIndex;
+    },
+
+    handleReset(clearFilters) {
+      clearFilters();
+      this.searchText = '';
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
